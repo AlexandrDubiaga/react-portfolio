1f14a76a290e043943da8ff3a2cdec6224dcc377
Alex <alexandr.dubiaga@gmail.com>
2019-11-04 11:38:03 +0200

    created projects component and css styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styles from './App.module.css';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Skills from "./components/Skills/Skills";
+import Projects from "./components/Projects/Projects";
 
 const App =() => {
     return (
@@ -11,6 +12,7 @@ const App =() => {
                 <Header />
                 <Main />
                 <Skills />
+                <Projects />
             </div>
         </div>
     );
