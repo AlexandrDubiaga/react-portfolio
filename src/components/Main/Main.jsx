0d183443860ac0de1974a8f9ae45da1109efe9a8
Alex <alexandr.dubiaga@gmail.com>
2019-11-04 16:16:50 +0200

    finishing main block flex

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -4,7 +4,16 @@ import styles from './Main.module.css';
 const Main = () => {
     return (
         <div className={styles.main}>
-            Main
+            <div className={styles.container}>
+                <div className={styles.greeting}>
+                    <span>Привет</span>
+                    <span>Меня зовут,иван Иванов</span>
+                    <span>Я астронавт</span>
+                </div>
+                <div className={styles.photo}>
+                    <img src="" alt=""/>
+                </div>
+            </div>
         </div>
     );
 }
