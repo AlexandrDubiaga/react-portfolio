32ce0fe58e566b2f240bbd903bed411c93c2b753
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2019-11-27 18:50:28 +0200

    Updates

---
+++ precache-manifest.f03cbdf6b88fd9a2ba1980e716dc1cc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cec803f788974de665e9621fe295d459",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "80f1eab5e79971dbedcb",
+    "url": "/react-portfolio/static/css/main.4cec56d3.chunk.css"
+  },
+  {
+    "revision": "fbd30f204b9eab63134b",
+    "url": "/react-portfolio/static/js/2.5701127c.chunk.js"
+  },
+  {
+    "revision": "80f1eab5e79971dbedcb",
+    "url": "/react-portfolio/static/js/main.9ec439b0.chunk.js"
+  },
+  {
+    "revision": "175a75cdfaf1231de18b",
+    "url": "/react-portfolio/static/js/runtime-main.98067e24.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.29a4595082a2b6cc078d6d38d56a8842.js"
+  "/react-portfolio/precache-manifest.f03cbdf6b88fd9a2ba1980e716dc1cc0.js"
 );
 
 self.addEventListener('message', (event) => {
