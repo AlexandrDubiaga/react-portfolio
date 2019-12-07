ca9d6121b421fec7617142f690b535f6f541d1af
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2019-12-07 11:42:54 +0200

    Updates

--- precache-manifest.a9eeb633ee94ceeb95456eea43ad42f1.js
+++ precache-manifest.a9eeb633ee94ceeb95456eea43ad42f1.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d59f9a1cea43e3ecc583bce22b5627b2",
+    "revision": "296a2c5c533328e4c12f869400e0d30a",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "ee599e1640a6e548890c",
-    "url": "/react-portfolio/static/css/main.6a4b7cd9.chunk.css"
+    "revision": "eec37523c765854e5e82",
+    "url": "/react-portfolio/static/css/main.a42948a7.chunk.css"
   },
   {
     "revision": "fbd30f204b9eab63134b",
     "url": "/react-portfolio/static/js/2.5701127c.chunk.js"
   },
   {
-    "revision": "ee599e1640a6e548890c",
-    "url": "/react-portfolio/static/js/main.c7fbc235.chunk.js"
+    "revision": "eec37523c765854e5e82",
+    "url": "/react-portfolio/static/js/main.e9e80d8b.chunk.js"
   },
   {
     "revision": "175a75cdfaf1231de18b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.20a2985e268d240ccc3b464290b80cf7.js"
+  "/react-portfolio/precache-manifest.a9eeb633ee94ceeb95456eea43ad42f1.js"
 );
 
 self.addEventListener('message', (event) => {
