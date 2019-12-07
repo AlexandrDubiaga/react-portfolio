7c12214281908fd7d2ccfad3251ea08bd8657af5
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2019-12-07 13:43:33 +0200

    Updates

--- precache-manifest.52784efd8ef9019d4b985794acfe57f5.js
+++ precache-manifest.52784efd8ef9019d4b985794acfe57f5.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c7ac16d7cbfd31bd5b010c3836288c22",
+    "revision": "f5ca48d631e521f54e33dddcc8d75377",
     "url": "/react-portfolio/index.html"
   },
   {
-    "revision": "8f105e1d3c266c187e51",
-    "url": "/react-portfolio/static/css/main.041f4be9.chunk.css"
+    "revision": "394e7614c26b2ead225d",
+    "url": "/react-portfolio/static/css/main.8d2f3c63.chunk.css"
   },
   {
     "revision": "fbd30f204b9eab63134b",
     "url": "/react-portfolio/static/js/2.5701127c.chunk.js"
   },
   {
-    "revision": "8f105e1d3c266c187e51",
-    "url": "/react-portfolio/static/js/main.1e3c3a26.chunk.js"
+    "revision": "394e7614c26b2ead225d",
+    "url": "/react-portfolio/static/js/main.1e789385.chunk.js"
   },
   {
     "revision": "175a75cdfaf1231de18b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.3d7d8aab9e5389cc855168228eb9c004.js"
+  "/react-portfolio/precache-manifest.52784efd8ef9019d4b985794acfe57f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
