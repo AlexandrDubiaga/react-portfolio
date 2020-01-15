3a866385c8204daf1a31420aeb8f59eca996737a
Alex <alexandr.dubiaga@gmail.com>
2020-01-15 11:11:06 +0200

    fe

--- src/components/Skills/SkillsItem.jsx
+++ src/components/Skills/SkillsItem.jsx
@@ -4,8 +4,12 @@ import styles from './Skills.module.css';
 const SkillsItem = props => {
     return (
         <div className={styles.photosBlocks}>
-            <img src={props.imgSrc}/>
-            <span className={styles.descPhotos}>{props.title}</span>
+            <div>
+                <img src={props.imgSrc}/>
+            </div>
+            <div>
+                <span className={styles.descPhotos}>{props.title}</span>
+            </div>
         </div>
     )
 }
