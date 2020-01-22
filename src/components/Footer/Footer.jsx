093ed914e2752594231397417f22c31fcabe941e
Alex <alexandr.dubiaga@gmail.com>
2020-01-22 15:03:45 +0200

    finish footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,10 +9,10 @@ const Footer = () => {
                     <span>Александр Дубяга</span>
                 </div>
                 <div className={styles.main}>
-                    <div>Facebook</div>
-                    <div>Twitter</div>
-                    <div>Vk</div>
-                    <div>Instagram</div>
+                    <a href="https://www.facebook.com/profile.php?id=100002788131536"> <div className={styles.facebook}></div></a>
+                    <a href="https://twitter.com/Solmir16"> <div className={styles.twitter}></div></a>
+                    <a href="https://vk/alexcardo"> <div className={styles.vk}></div></a>
+                    <a href="https://www.instagram.com/solmir__/?hl=ru"> <div className={styles.instagram}></div></a>
                 </div>
                 <div className={styles.acsess}>
                     <span>2019 Все права защищены</span>
