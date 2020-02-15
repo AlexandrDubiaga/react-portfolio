6b0b42169926b6243b940da2c2ac7e1c6a7598ca
LiudmylaV <lydmilavodopyan@gmail.com>
2020-02-15 12:19:07 +0200

    format footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,24 +7,31 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <Fade left>
-            <div className={styles.content}>
-                <ScrollableAnchor id={'contacts'}>
-                <div className={styles.title}>
-                    <span>Александр Дубяга</span>
+                <div className={styles.content}>
+                    <ScrollableAnchor id={'contacts'}>
+                        <div className={styles.title}>
+                            <span>Александр Дубяга</span>
+                        </div>
+                    </ScrollableAnchor>
+                    <div className={styles.main}>
+                        <a href="https://www.facebook.com/profile.php?id=100002788131536">
+                            <div className={styles.facebook}></div>
+                        </a>
+                        <a href="https://twitter.com/Solmir16">
+                            <div className={styles.twitter}></div>
+                        </a>
+                        <a href="https://vk/alexcardo">
+                            <div className={styles.vk}></div>
+                        </a>
+                        <a href="https://www.instagram.com/solmir__/?hl=ru">
+                            <div className={styles.instagram}></div>
+                        </a>
+                    </div>
+                    <div className={styles.acsess}>
+                        <span>2019 Все права защищены</span>
+                    </div>
                 </div>
-                </ScrollableAnchor>
-                <div className={styles.main}>
-                    <a href="https://www.facebook.com/profile.php?id=100002788131536"> <div className={styles.facebook}></div></a>
-                    <a href="https://twitter.com/Solmir16"> <div className={styles.twitter}></div></a>
-                    <a href="https://vk/alexcardo"> <div className={styles.vk}></div></a>
-                    <a href="https://www.instagram.com/solmir__/?hl=ru"> <div className={styles.instagram}></div></a>
-                </div>
-                <div className={styles.acsess}>
-                    <span>2019 Все права защищены</span>
-                </div>
-
-            </div>
-        </Fade>
+            </Fade>
         </div>
     );
 }
