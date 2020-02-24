1576d9a171c13de280550d363c20241961f58fd8
Alex <alexandr.dubiaga@gmail.com>
2020-02-24 16:54:37 +0200

    valid form contact

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Contacts.module.css';
 import Fade from 'react-reveal/Fade';
 import ScrollableAnchor from 'react-scrollable-anchor'
@@ -7,30 +7,50 @@ let nodemailer = require('nodemailer');
 
 
 const Contacts = () => {
+    const [message, sendMessage] = useState(false);
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [text, setText] = useState('');
+
     let sendForm = (e) => {
         e.preventDefault();
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const message = document.getElementById('text').value;
-        axios.post(`https://app-send.herokuapp.com/send`,{name: name, email: email, message: message}
-        ).then(() => {
-                alert("Message Sent.");
-        }).catch(e=>{
-        })
+        if (!name) {
+            sendMessage('Полe имя должно быть заполнено');
+        }else if (!email) {
+            sendMessage('Полe email должно быть заполнено');
+        } else if (!text) {
+            sendMessage('Полe text должно быть заполнено');
+        } else {
+            axios.post(`https://app-send.herokuapp.com/send`, {name: name, email: email, message: text}
+            ).then(() => {
+                sendMessage('Сообщение отправленно');
+                setName('');
+                setEmail('');
+                setText('');
+            }).catch(e => {
+                sendMessage('Ошибка' + e);
+            })
+        }
     }
     return (
         <div className={styles.contacts}>
             <Fade right>
-
                 <div className={styles.formContent}>
                     <ScrollableAnchor id={'contacts'}>
                         <div className={styles.titleContacts}>Контакты</div>
                     </ScrollableAnchor>
                     <div className={styles.form}>
                         <form>
-                            <div className={styles.inputs}><input placeholder={"Имя"} id="name" type="text"/></div>
-                            <div className={styles.inputs}><input placeholder={"Email"} id="email" type="text"/></div>
-                            <div className={styles.inputs}><textarea placeholder={"Сообщение..."} id="text"
+                            <div className={styles.inputs}>{message}</div>
+                            <div className={styles.inputs}><input value={name}
+                                                                  onChange={(e) => setName(e.currentTarget.value)}
+                                                                  placeholder={"Имя"} id="name" type="text"/></div>
+                            <div className={styles.inputs}><input value={email} placeholder={"Email"}
+                                                                  onChange={(e) => setEmail(e.currentTarget.value)}
+                                                                  id="email" type="text"/></div>
+                            <div className={styles.inputs}><textarea value={text}
+                                                                     onChange={(e) => setText(e.currentTarget.value)}
+                                                                     placeholder={"Сообщение..."} id="text"
                                                                      type="text"/>
                             </div>
                             <div>
